8ea0e55c26cf0f6599663fe6e32c4a68bf8b5350
AnFaydi <anfaydi1@yandex.ru>
2024-09-13 22:31:50 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,15 @@
+my_dict = {'Anton': 2005, 'Egor': 1995, 'Denis': 1844}
+print(my_dict)
+print (my_dict['Anton'])
+my_dict ['Daniil'] = 2010
+print (my_dict['Daniil'])
+my_dict.update({'Masha': 2006, 'Katya': 2018})
+print(my_dict.pop('Egor'))
+print(my_dict)
+my_set = {5,4,2,'String', 4,5,2,True,False,True,'String'}
+print(my_set)
+my_set.add(6)
+my_set.add('STR')
+print(my_set)
+my_set.discard('String')
+print(my_set)
